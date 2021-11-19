6c4e645ba518536c011629be8e54420b73682d0c
Dathan Stone <developer.stone@outlook.com>
2021-11-18 22:42:06 -0700

    Initial Commit

---
+++ src/test/java/com/devjob/devjobtrackerbackend/DevJobTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjob.devjobtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJobTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
